edd3606d27157b7db2bf7c266975d322e695657d
Lai Chun Pong <laichunpongben@gmail.com>
2017-05-31 14:12:22 +0800

    Add make_thumbnail, make_dataset, load_data

--- pixiv_loader.py
+++ pixiv_loader.py
@@ -1,8 +1,15 @@
 from __future__ import print_function
 import os
+import sys
 import json
 import time
 import pixivpy3
+if sys.version_info >= (3, 0):
+    import pickle
+else:
+    import cPickle as pickle
+import numpy as np
+from PIL import Image
 
 
 class PixivLoader(pixivpy3.PixivAPI):
@@ -43,7 +50,51 @@ class PixivLoader(pixivpy3.PixivAPI):
 
         print('Total {} illusts saved'.format(illust_count))
 
+    def make_thumbnail(self, infile):
+        size = 128, 128
+        outfile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                              'thumbnails',
+                              os.path.splitext(os.path.basename(infile))[0] +
+                              '.thumbnail' +
+                              os.path.splitext(os.path.basename(infile))[1])
+        try:
+            image = Image.open(infile)
+            image.thumbnail(size, Image.ANTIALIAS)
+            background = Image.new('RGBA', size, (255, 255, 255, 0))
+            background.paste(
+                image, (int((size[0] - image.size[0]) / 2), int((size[1] - image.size[1]) / 2))
+            )
+            background.save(outfile, "JPEG")
+        except IOError:
+            raise
+
+    def make_dataset(self):
+        image_ext = ['.jpg', '.png', '.gif']
+        for root, dirs, files in os.walk(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                         'illusts')):
+            for f in files:
+                if f.endswith(tuple(image_ext)):
+                    self.make_thumbnail(os.path.join(root, f))
+
+    def serialize(self):
+        np.savez('pixiv.npz', x, y)
+
+    def load_data(self):
+        fpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'datasets',
+                             'pixiv.npz')
+        try:
+            f = np.load(path)
+            x_train = f['x_train']
+            y_train = f['y_train']
+            x_test = f['x_test']
+            y_test = f['y_test']
+        finally:
+            f.close()
+        return (x_train, y_train), (x_test, y_test)
+
 if __name__ == '__main__':
     artist_id = 946272
     pixiv_loader = PixivLoader()
-    pixiv_loader.save_illusts(artist_id)
+    # pixiv_loader.save_illusts(artist_id)
+    pixiv_loader.make_dataset()
